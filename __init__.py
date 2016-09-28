a5da12b1ecc959bfbedf18b0e84627f6f6f82353
Joshua Koike <jkoike@gmail.com>
2016-09-28 14:32:34 -0700

    Crude vocabulary, passing entire message as query

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
 from .. import Skill
 import wolframalpha
 import os
+import asyncio
 
 class SearchSkill(Skill):
     async def on_load(self):
         self.client = wolframalpha.Client(os.environ["ALPHA_KEY"])
 
-    async def parse(self, message, question, phrase, conjunction=""):
-        res = await asyncio.get_event_loop().run_in_executor(None, self.client.query, phrase)
-        return "According to WolframAlpha:\n```"+next(res.results).text+"```"
+    async def parse(self, message, question):
+        res = await asyncio.get_event_loop().run_in_executor(None, self.client.query, message.clean_content)
+        try:
+            return "According to WolframAlpha:\n```"+next(res.results).text+"```"
+        except:
+            return "I asked Wolfram Alpha, but they returned no answers. Try rephrasing your question."
